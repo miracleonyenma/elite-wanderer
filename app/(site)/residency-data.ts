91854159a2098501bf862ae7396da86fe542e792
miracleonyenma <miracleficient@gmail.com>
2026-01-02 14:09:21 +0100

    feat: update and expand `residencyPrograms` data with new citizenship and residency options.

--- app/(site)/residency-data.ts
+++ app/(site)/residency-data.ts
@@ -1,45 +1,34 @@
 export const residencyPrograms = [
+  // Citizenship Programs
   {
-    location: "Portugal",
-    type: "Residency by investment program",
-    price: "Starting from € 500,000",
-    image: "/images/global-residency/Portugal-small.jpg",
-  },
-  {
-    location: "Grenada",
+    location: "Saint Kitts and Nevis",
     type: "Citizenship by investment program",
-    price: "Starting from $ 235,000",
-    image: "/images/global-residency/Grenada-small.jpg",
-  },
-  // {
-  //   location: "Vanuatu",
-  //   type: "Citizenship by investment program",
-  //   price: "Starting from $ 130,000",
-  //   image: "/images/global-residency/Vanatu-small.jpg",
-  // },
-  // {
-  //   location: "Turkey",
-  //   type: "Citizenship by investment program",
-  //   price: "Starting from $ 400,000",
-  //   image: "/images/global-residency/Turkey-small.jpg",
-  // },
-  {
-    location: "Greece",
-    type: "Residency by investment program",
-    price: "Starting from € 250,000",
-    image: "/images/global-residency/Greece-small.jpg",
+    price: "Starting from $ 250,000",
+    image: "/images/pexels-8pcarlos-morocho-2150734957-35260825.webp",
   },
   {
-    location: "Malta",
-    type: "Residency by investment program",
-    price: "Starting from € 135,000",
-    image: "/images/global-residency/malta.jpg",
-  },
-  {
-    location: "St. Lucia",
+    location: "Saint Lucia",
     type: "Citizenship by investment program",
     price: "Starting from $ 100,000",
     image: "/images/global-residency/244205-St-Lucia.jpg",
+  },
+  {
+    location: "Turkey",
+    type: "Citizenship by investment program",
+    price: "Starting from $ 400,000",
+    image: "/images/global-residency/Turkey-small.jpg",
+  },
+  {
+    location: "Vanuatu",
+    type: "Citizenship by investment program",
+    price: "Starting from $ 130,000",
+    image: "/images/global-residency/Vanatu-small.jpg",
+  },
+  {
+    location: "Antigua & Barbuda",
+    type: "Citizenship by investment program",
+    price: "Starting from $ 100,000",
+    image: "/images/pexels-geoffrey-currie-2153251493-33128243.webp",
   },
   {
     location: "Dominica",
@@ -48,15 +37,53 @@ export const residencyPrograms = [
     image: "/images/global-residency/dominica-.png.webp",
   },
   {
-    location: "Dubai, UAE",
+    location: "Grenada",
+    type: "Citizenship by investment program",
+    price: "Starting from $ 235,000",
+    image: "/images/global-residency/Grenada-small.jpg",
+  },
+  {
+    location: "Malta (Citizenship)",
+    type: "Citizenship by investment program",
+    price: "Starting from € 600,000",
+    image: "/images/global-residency/malta.jpg",
+  },
+
+  // Residency Programs
+  {
+    location: "Portugal",
     type: "Residency by investment program",
-    price: "Starting from $ 550,000",
-    image: "/images/global-residency/uae.jpg.webp",
+    price: "Starting from € 500,000",
+    image: "/images/global-residency/Portugal-small.jpg",
   },
   {
     location: "United Kingdom",
     type: "Residency by investment program",
     price: "Starting from £ 2,000,000",
     image: "/images/global-residency/london-blog-1.jpg",
+  },
+  {
+    location: "Greece",
+    type: "Residency by investment program",
+    price: "Starting from € 250,000",
+    image: "/images/global-residency/Greece-small.jpg",
+  },
+  {
+    location: "Hungary",
+    type: "Residency by investment program",
+    price: "Starting from € 250,000",
+    image: "/images/pexels-mahmudul-hasan-2004633-35257841.webp",
+  },
+  {
+    location: "Latvia",
+    type: "Residency by investment program",
+    price: "Starting from € 50,000",
+    image: "/images/pexels-rada-aslanova-150604297-34567860.webp",
+  },
+  {
+    location: "Malta (Residency)",
+    type: "Residency by investment program",
+    price: "Starting from € 135,000",
+    image: "/images/global-residency/malta.jpg",
   },
 ];
